e16aa296eac121d290c3cf15678f6deb37123a93
Severin Rohner <severin.rohner@clavisit.com>
2016-12-12 16:04:47 +0100

    Add portal update function to onClick

--- modules/soy-test-portlet/src/main/resources/META-INF/resources/View.es.js
+++ modules/soy-test-portlet/src/main/resources/META-INF/resources/View.es.js
@@ -2,10 +2,16 @@ import Component from 'metal-component/src/Component';
 import Soy from 'metal-soy/src/Soy';
 import templates from './View.soy';
 
+import core from 'metal/src/core';
+import dom from 'metal-dom/src/dom';
+
 class View extends Component {
+
     constructor(opt_config) {
-        console.log("constructor");
         super(opt_config);
+        console.log("constructor");
+        this.gaVersion = 3;
+        this.minorVersion = 2;
     }
 
     /**
@@ -15,8 +21,15 @@ class View extends Component {
         console.log('attached');
     }
 
+
+
     myClickFunction() {
-        alert('Jipi');
+        let releaseInfo = this.releaseInfo.replace("7.0.".concat(this.minorVersion.toString()),
+            "7.0.".concat((++this.minorVersion).toString()));
+        releaseInfo = releaseInfo.replace("GA".concat(this.gaVersion).toString(),
+            "GA".concat((++this.gaVersion).toString()));
+        this.releaseInfo = releaseInfo;
+        console.log('Click!!!');
     }
 }
 
@@ -26,7 +39,7 @@ Soy.register(View, templates);
 View.STATE = {
     releaseInfo: {
         // The default value will be: `This is my default STATE value, yeah`.
-        value: 'This is my default STATE value, yeah'
+        value: 0
     }
 }
 
